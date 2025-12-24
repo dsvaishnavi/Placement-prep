21c29fd72eeea6d43da3016a8879a34f332ca9d2
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-12-24 21:43:19 +0530

    backend router

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
-import express from "express" ;
+import express from "express";
 import dotenv from "dotenv";
-import cors from "cors" ;
+import cors from "cors";
 import connectDB from "./config/db.js";
+import router from "./Routes/authrouter.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use("/", router);
 
 //MIDDLEWARE
 app.use(cors());
@@ -14,11 +17,10 @@ app.use(express.json());
 
 //Routes
 app.get("/", (req, res) => {
-    res.send("Our Server Is In Running Position");
-  });
+  res.send("Our Server Is In Running Position");
+});
 
 const PORT = process.env.PORT;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port: http://localhost:${PORT}`);
-  });
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}`);
+});
